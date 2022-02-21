da6926bba97c51f1e8164dcc54384210d5a95323
olo135597 <olo135597@stud.gibb.ch>
2022-02-21 16:01:00 +0100

    Created Table OwnedProduct

--- Skripts/Student_002/OwnedProduct.sql
+++ Skripts/Student_002/OwnedProduct.sql
@@ -1,1 +1,15 @@
 --DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
+use test
+IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='OwnedProduct' and xtype='U')
+	Create Table OwnedProduct(
+	LicenceHash varchar(30) NOT NULL UNIQUE
+	)
+GO
+
+/*
+SELECT * FROM OwnedProduct;
+
+INSERT INTO OwnedProduct(LicenceHash)
+Values('Hello World');
+
+DROP TABLE OwnedProduct;
