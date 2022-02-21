7b04ea542f125c0d5b8e3e884cbee43454f06f6e
daviddresus <99662712+daviddresus@users.noreply.github.com>
2022-02-21 14:13:38 +0100

    Update Image.sql

--- Skripts/Student_004/Image.sql
+++ Skripts/Student_004/Image.sql
@@ -1,1 +1,11 @@
 --DEFALUT USER IDENTIFIER: dro135638 <-- DO NOT MODIFY
+use test
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name="Image" and xtype="U")
+
+   CREATE TABLE Image
+   (
+      ImageID int PRIMARY KEY IDENTITY,
+      ProductID int FOREIGN KEY,
+      ImageFile blob
+   )
+go
