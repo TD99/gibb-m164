485582787f7adca549da1002992716205f22d662
olo135597 <olo135597@stud.gibb.ch>
2022-02-21 15:24:36 +0100

    Payment Tabelle gemacht

--- Skripts/Student_003/Payment.sql
+++ Skripts/Student_003/Payment.sql
@@ -1,24 +1,17 @@
 --DEFALUT USER IDENTIFIER: olo135597 <-- DO NOT MODIFY
-use test;
-IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Product' and xtype='U')
-	Create Table Product(
-		ProductID int PRIMARY KEY identity, 
-		Name varchar(50) NOT NULL,
-		DownloadURL nvarchar(300) NOT NULL,
-		Description varchar(500),
-		ReleaseYear SMALLINT,
-		Price SMALLMONEY NOT NULL,
-		MinAge TINYINT NOT NULL,
-		RemoveDate DATE
+use test
+IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Payment' and xtype='U')
+	Create Table Payment(
+	PaymentID int identity Primary Key,
+	Amount SMALLMONEY,
 	)
-GO 
-
+GO
 
 /* Beispiele
-SELECT * FROM Product;
+SELECT * FROM Payment;
 
-Insert Into Product (Name,DownloadURL,Description,ReleaseYear,Price, MinAge,RemoveDate)
-Values('Mario','Jump and Run','LOL',2022,10,18,'2022-02-21');
+Insert Into Payment(Amount)
+Values(200);
 
-DROP TABLE Product;
+DROP TABLE Payment;
 */
