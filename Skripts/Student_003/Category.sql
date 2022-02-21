1efcc9be81aa4520cf39f2e86019dfe813725f99
olo135597 <olo135597@stud.gibb.ch>
2022-02-21 14:12:49 +0100

    Update Category.sql

--- Skripts/Student_003/Category.sql
+++ Skripts/Student_003/Category.sql
@@ -1,1 +1,17 @@
 --DEFALUT USER IDENTIFIER: olo135597 <-- DO NOT MODIFY
+use test
+IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Category' and xtype='U')
+	Create Table Category(
+	CategoryID int identity Primary Key,
+	Name varchar(50) NOT NULL UNIQUE,
+	Description varchar(500),
+	)
+GO
+
+/* Beispiele
+SELECT * FROM Category;
+
+Insert Into Category (Name,Description)
+Values('Jump and Run','Eine berühmte Spielart');
+
+DROP TABLE Category;
