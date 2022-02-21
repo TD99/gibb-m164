2e88b100336675b83c77207d0baf1556c6ba8b26
olo135597 <olo135597@stud.gibb.ch>
2022-02-21 15:05:08 +0100

    Created PaymentMethod table

--- Skripts/Student_003/PaymentMethod.sql
+++ Skripts/Student_003/PaymentMethod.sql
@@ -1,1 +1,17 @@
 --DEFALUT USER IDENTIFIER: olo135597 <-- DO NOT MODIFY
+use test
+IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='PaymentMethod' and xtype='U')
+	Create Table PaymentMethod(
+	PaymentMethodID int identity Primary Key,
+	Name varchar(50) NOT NULL UNIQUE,
+	)
+GO
+
+/* Beispiele
+SELECT * FROM PaymentMethod;
+
+Insert Into PaymentMethod(Name)
+Values('Jump and Run');
+
+DROP TABLE PaymentMethod;
+*/
