d9cbbe899340cd56aa2a22b502c53f3c8cec3d8b
olo135597 <olo135597@stud.gibb.ch>
2022-02-21 15:54:46 +0100

    Create Coupon Table

--- Skripts/Student_002/Coupon.sql
+++ Skripts/Student_002/Coupon.sql
@@ -1,1 +1,20 @@
---DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
+--DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY 
+use test
+IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Coupon' and xtype='U')
+	Create Table Coupon(
+	CouponID int identity Primary Key,
+	Code varchar(50) NOT NULL UNIQUE,
+	StartDate Date NOT NULL,
+	EndDate Date NOT NULL,
+	Percentage REAL NOT NULL,
+	)
+GO
+
+/* 
+SELECT * FROM Coupon; 
+
+INSERT INTO Coupon(Code,StartDate,EndDate,Percentage)
+Values('Hello World','2022-01-22','2022-02-22',89.6); 
+
+DROP TABLE Coupon;
+*/
