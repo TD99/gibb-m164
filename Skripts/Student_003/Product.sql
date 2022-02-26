ef64d481468e03d4d9722edf96ebc96d23ae832b
Tim Dürr <tim@duerrs.ch>
2022-02-26 18:21:10 +0100

    Update Product.sql

--- Skripts/Student_003/Product.sql
+++ Skripts/Student_003/Product.sql
@@ -1,24 +1,27 @@
 --DEFALUT USER IDENTIFIER: olo135597 <-- DO NOT MODIFY
-use test;
-IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Product' and xtype='U')
-	Create Table Product(
-		ProductID int PRIMARY KEY identity, 
-		Name varchar(50) NOT NULL,
-		DownloadURL nvarchar(300) NOT NULL,
-		Description varchar(500),
-		ReleaseYear SMALLINT,
-		Price SMALLMONEY NOT NULL,
-		MinAge TINYINT NOT NULL,
-		RemoveDate DATE
-	)
-GO 
 
+--TABLE PRODUCT
+	USE test;
+	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Product' and xtype='U')
+		CREATE TABLE Product(
+			ProductID INT PRIMARY KEY IDENTITY, 
+			Name VARCHAR(50) NOT NULL,
+			DownloadURL NVARCHAR(300) NOT NULL,
+			Description VARCHAR(500),
+			ReleaseYear SMALLINT,
+			Price SMALLMONEY NOT NULL,
+			MinAge TINYINT NOT NULL,
+			RemoveDate DATE
+		)
+	GO;
 
-/* Beispiele
-SELECT * FROM Product;
+	/* Examples:
+	SELECT * FROM Product;
 
-Insert Into Product (Name,DownloadURL,Description,ReleaseYear,Price, MinAge,RemoveDate)
-Values('Mario','Jump and Run','LOL',2022,10,18,'2022-02-21');
+	Insert Into Product (Name,DownloadURL,Description,ReleaseYear,Price,MinAge,RemoveDate)
+	Values('Mario','Jump and Run','LOL',2022,10,18,'2022-02-21');
 
-DROP TABLE Product;
-*/
+	DROP TABLE Product;
+	*/
+
+--END
