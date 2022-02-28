acee9b293d0c56b464d37d8470b7f56d5390c649
T-Craft <tim@duerrs.ch>
2022-02-28 12:06:01 +0100

    ...

--- Skripts/Student_001/Country.sql
+++ Skripts/Student_001/Country.sql
@@ -1,18 +1,18 @@
 --DEFALUT USER IDENTIFIER: tdu135585 <-- DO NOT MODIFY
 
 --CREATE TABLE COUNTRY
-	USE test;
+	USE "test";
 	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Country' AND xtype='U')
 		CREATE TABLE "Country"(
-			CountryID INT PRIMARY KEY IDENTITY,
-			Name VARCHAR(100) NOT NULL UNIQUE,
+			"CountryID" INT PRIMARY KEY IDENTITY,
+			"Name" VARCHAR(100) NOT NULL UNIQUE,
 		);
 	GO
 
 	/* Examples:
 	SELECT * FROM "Country";
 
-	INSERT INTO "Country" (Name)
+	INSERT INTO "Country" ("Name")
 	VALUES('Switzerland');
 
 	DROP TABLE "Country";
--- Skripts/Student_001/Customer.sql
+++ Skripts/Student_001/Customer.sql
@@ -1,24 +1,24 @@
 --DEFALUT USER IDENTIFIER: tdu135585 <-- DO NOT MODIFY
 
 --CREATE TABLE CUSTOMER
-	USE test;
+	USE "test";
 	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Customer' AND xtype='U')
 		CREATE TABLE "Customer"(
-			CustomerID INT PRIMARY KEY IDENTITY,
-			FirstName VARCHAR(50),
-			LastName VARCHAR(50) NOT NULL,
-			Birthday DATE NOT NULL,
-			Gender VARCHAR(1) NOT NULL,
-			Email VARCHAR(200),
-			PhoneHome VARCHAR(50),
-			PhoneMobile VARCHAR(50),
+			"CustomerID" INT PRIMARY KEY IDENTITY,
+			"FirstName" VARCHAR(50),
+			"LastName" VARCHAR(50) NOT NULL,
+			"Birthday" DATE NOT NULL,
+			"Gender" VARCHAR(1) NOT NULL,
+			"Email" VARCHAR(200),
+			"PhoneHome" VARCHAR(50),
+			"PhoneMobile" VARCHAR(50),
 		);
 	GO
 
 	/* Examples:
 	SELECT * FROM "Customer";
 
-	INSERT INTO "Customer" (FirstName,LastName,Birthday,Gender,Email,PhoneHome,PhoneMobile)
+	INSERT INTO "Customer" ("FirstName","LastName","Birthday","Gender","Email","PhoneHome","PhoneMobile")
 	VALUES('Max','Mustermann','2019-01-02','m','max.mustermann@gmail.com','+41 31 334 56 43','+41 79 553 66 44');
 
 	DROP TABLE "Customer";
