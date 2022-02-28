b8a61c61f5ca3209d310cb0f92ac1b91ba0bd170
olo135597 <olo135597@stud.gibb.ch>
2022-02-28 12:05:28 +0100

    Update Order.sql

--- Skripts/Student_002/Order.sql
+++ Skripts/Student_002/Order.sql
@@ -1,14 +1,14 @@
 --DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
 
 --CREATE TABLE ORDER
-   USE test
+   USE "test"
 	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Order' AND xtype='U')
 		CREATE TABLE "Order"(
-		OrderID INT PRIMARY KEY IDENTITY, 
-		BuyDate Date NOT NULL, 
-		IsPaid BIT NOT NULL, 
-		ReturnDate Date, 
-	)
+		"OrderID" INT PRIMARY KEY IDENTITY, 
+		"BuyDate" Date NOT NULL, 
+		"IsPaid" BIT NOT NULL, 
+		"ReturnDate" Date,  
+	);
 GO
 /* 
 	SELECT * FROM "Order"; 
