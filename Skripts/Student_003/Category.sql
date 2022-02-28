fd133d3e32218844b86778a84ba72f7b4f324f8f
T-Craft <tim@duerrs.ch>
2022-02-28 12:10:58 +0100

    Update Category.sql

--- Skripts/Student_003/Category.sql
+++ Skripts/Student_003/Category.sql
@@ -1,21 +1,21 @@
 --DEFALUT USER IDENTIFIER: olo135597 <-- DO NOT MODIFY
 
 --CREATE TABLE CATEGORY
-	USE test;
+	USE "test";
 	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Category' AND xtype='U')
-		CREATE TABLE Category(
-			CategoryID INT PRIMARY KEY IDENTITY,
-			Name VARCHAR(50) NOT NULL UNIQUE,
-			Description VARCHAR(500)
+		CREATE TABLE "Category"(
+			"CategoryID" INT PRIMARY KEY IDENTITY,
+			"Name" VARCHAR(50) NOT NULL UNIQUE,
+			"Description" VARCHAR(500),
 		);
 	GO
 
 	/* Examples:
-	SELECT * FROM Category;
+	SELECT * FROM "Category";
 
-	INSERT INTO Category (Name,Description)
+	INSERT INTO "Category"("Name","Description")
 	VALUES('Jump and Run','Eine berühmte Spielart');
 
-	DROP TABLE Category;
+	DROP TABLE "Category";
 	*/
 --END
