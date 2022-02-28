041ae854bcbbefa1325fa001acedc3b95f6e19a4
T-Craft <tim@duerrs.ch>
2022-02-28 12:08:02 +0100

    Update Coupon.sql

--- Skripts/Student_002/Coupon.sql
+++ Skripts/Student_002/Coupon.sql
@@ -1,23 +1,23 @@
 --DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
 
 --CREATE TABLE COUPON
-	USE test
+	USE "test";
 	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Coupon' AND xtype='U')
-		CREATE TABLE Coupon(
-		CouponID INT PRIMARY KEY IDENTITY,
-		Code VARCHAR(50) NOT NULL UNIQUE,
-		StartDate DATE NOT NULL,
-		EndDate DATE,
-		Percentage REAL NOT NULL,
-		)
+		CREATE TABLE "Coupon"(
+			"CouponID" INT PRIMARY KEY IDENTITY,
+			"Code" VARCHAR(50) NOT NULL UNIQUE,
+			"StartDate" DATE NOT NULL,
+			"EndDate" DATE,
+			"Percentage" REAL NOT NULL,
+		);
 	GO
 
 	/* 
-	SELECT * FROM Coupon; 
+	SELECT * FROM "Coupon"; 
 
-	INSERT INTO Coupon(Code,StartDate,EndDate,Percentage)
+	INSERT INTO "Coupon"("Code","StartDate","EndDate","Percentage")
 	VALUES('Hello World','2022-01-22','2022-02-22',89.6); 
 
-	DROP TABLE Coupon;
+	DROP TABLE "Coupon";
 	*/
 --END
