511f321889ed649280930d7fab51c2eef2b86a19
olo135597 <olo135597@stud.gibb.ch>
2022-02-28 13:13:28 +0100

    Update Publisher.sql

--- Skripts/Student_004/Publisher.sql
+++ Skripts/Student_004/Publisher.sql
@@ -5,14 +5,16 @@
 	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Publisher' AND xtype='U')
 		CREATE TABLE "Publisher"(
 			"PublisherID" INT PRIMARY KEY IDENTITY,
-			"Name" VARCHAR(50),
-			"Website" VARCHAR(50),
+			"Name" VARCHAR(50) NOT NULL,
+			"Website" NVARCHAR(300),
 			"RemoveDate" DATE,
 		);
 	GO
 
 	/* Examples:
-	SELECT * FROM "Publisher";
+	SELECT * FROM "Publisher"; 
+	INSERT INTO "Publisher"("Name","Website","RemoveDate")
+	VALUES('Nintendo','Nintendo.com','2022-02-21')
 	DROP TABLE "Publisher";
 	*/
 --END
