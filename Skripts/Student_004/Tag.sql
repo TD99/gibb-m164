fed11b4ba7edbbe4174dc42ec231ed0945323aab
olo135597 <olo135597@stud.gibb.ch>
2022-02-28 13:16:30 +0100

    Update Tag.sql

--- Skripts/Student_004/Tag.sql
+++ Skripts/Student_004/Tag.sql
@@ -5,12 +5,14 @@
 	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Tag' AND xtype='U')
 		CREATE TABLE "Tag"(
 			"TagID" INT PRIMARY KEY IDENTITY,
-			"Name" VARCHAR(50),
+			"Name" VARCHAR(50) NOT NULL,
 		);
 	GO
 
 	/* Examples:
 	SELECT * FROM "Tag";
+	INSERT INTO "Tag"("Name")
+	VALUES('Example')
 	DROP TABLE "Tag";
 	*/
 --END
