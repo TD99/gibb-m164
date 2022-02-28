2513707ebb0d8879897f73aa1b4a37fa6c04c7ec
daviddresus <david.estevesrodrigues@bwdbern.ch>
2022-02-28 15:54:53 +0100

    Insert Done on Publisher and started on Platform

--- Skripts/Student_004/Insert/Platform.sql
+++ Skripts/Student_004/Insert/Platform.sql
@@ -2,6 +2,6 @@
 
 --INSERT INTO PLATFORM
 	USE "test";
-	INSERT INTO "Platform"(...)
-	VALUES(...);
+	INSERT INTO "Platform"("Name","MinSoftwareVersion","MaxSoftwareVersion")
+	VALUES('name','1.1.1','2.1.1');
 --END
