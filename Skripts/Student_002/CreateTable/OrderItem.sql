d5e7aadb0af9858472d004aceac5a0c7f98c2135
olo135597 <olo135597@stud.gibb.ch>
2022-03-06 12:09:34 +0100

    Student2 Forent Keys

--- Skripts/Student_002/CreateTable/OrderItem.sql
+++ Skripts/Student_002/CreateTable/OrderItem.sql
@@ -1,4 +1,15 @@
 --DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
 
 --CREATE TABLE ORDERITEM
+	USE "test";
+	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='OrderItem' AND xtype='U')
+		CREATE TABLE "OrderItem"( 
+			"FK_OrderID" INT,
+			"FK_ProductID" INT,
+			"FK_CouponID" INT,
+			"Quantity" INT NOT NULL, 
+			"TotalPricePerProduct" SMALLMONEY, 
+			 
+		);
+	GO
 --END
--- Skripts/Student_002/CreateTable/Wishlist.sql
+++ Skripts/Student_002/CreateTable/Wishlist.sql
@@ -1,4 +1,11 @@
 --DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
 
 --CREATE TABLE WISHLIST
+	USE "test";
+	IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Wishlist' AND xtype='U')
+		CREATE TABLE "Wishlist"(
+			"FK_CustomerID" INT,
+			"FK_ProductID" INT,
+		);
+	GO
 --END
