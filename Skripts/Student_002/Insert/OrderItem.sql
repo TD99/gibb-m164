4b299665956595a250f9f3253fc8f73af7d967c1
olo135597 <olo135597@stud.gibb.ch>
2022-03-07 12:21:34 +0100

    Updated Coupon

--- Skripts/Student_002/Insert/OrderItem.sql
+++ Skripts/Student_002/Insert/OrderItem.sql
@@ -1,4 +1,16 @@
 --DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
 
 --INSERT INTO ORDERITEM
+USE test;
+INSERT INTO "OrderItem"("Quantity","Price")
+	VALUES(2,30),
+	      (3,60),
+		  (1,40),
+		  (2,60),
+		  (1,40),
+		  (3,30),
+		  (1,50),
+		  (4,60),
+		  (2,80),
+		  (4,100);
 --END
