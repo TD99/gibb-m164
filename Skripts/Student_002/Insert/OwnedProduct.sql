49236ca1641da4b54d9fc77b44b90d61adcfe3a0
olo135597 <olo135597@stud.gibb.ch>
2022-03-07 14:25:19 +0100

    Update OwnedProduct.sql

--- Skripts/Student_002/Insert/OwnedProduct.sql
+++ Skripts/Student_002/Insert/OwnedProduct.sql
@@ -2,6 +2,21 @@
 
 --INSERT INTO OWNEDPRODUCT
 	USE "test";
-	INSERT INTO "OwnedProduct"(...)
-	VALUES(...);
+	INSERT INTO "OwnedProduct"("FK_CustomerID","FK_ProductID","LicenceHash")
+	VALUES(1,2,'BASDFKJHKJ'),
+	      (1,2,'BASFJEKJEK'),
+	      (2,3,'BKBDFKJAHF'),
+		  (2,3,'JDIEWKWLEI'),
+		  (2,4,'HKJHDFUE3A'),
+		  (3,1,'FKJ478938A'),
+		  (4,1,'HGKJHD34GH'),
+		  (4,1,'HFJDFUEUJE'),
+		  (5,5,'asdlkfjIDi'),
+		  (5,5,'asdflkjIie'),
+		  (5,5,'HFuehasdfu'),
+		  ('LKJFDLJIFE'),
+		  ('lkLKFjeaf7'),
+		  ('JFLJ30LFUH'),
+		  ('DJIEHGNGHE'),
+		  ('FHKJDUEH7J');
 --END
