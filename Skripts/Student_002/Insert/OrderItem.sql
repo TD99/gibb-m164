0f6af27c8779f1ed0bab97f29a83acab0d7d82eb
olo135597 <olo135597@stud.gibb.ch>
2022-03-13 15:38:39 +0100

    Update OrderItem.sql

--- Skripts/Student_002/Insert/OrderItem.sql
+++ Skripts/Student_002/Insert/OrderItem.sql
@@ -3,17 +3,17 @@
 --INSERT INTO ORDERITEM
 USE test;
 INSERT INTO "OrderItem"("FK_OrderID","FK_ProductID","Quantity","Price")
-	VALUES(1,2,2,30),
-	      (2,3,2,60),
-		  (2,4,1,40),
-		  (3,1,1,60),
-		  (4,1,2,60),
-		  (5,5,3,30),
-		  (5,7,1,50),
-		  (5,9,4,60),
-		  (6,3,2,60),
-		  (7,10,4,100),
-		  (8,2,1,30),
-		  (9,8,2,20),
-		  (10,6,1,80);
+	VALUES(1,2,2,52.90), --Uncharted
+	      (2,3,2,23), --Minecraft
+		  (2,4,1,59.99), --Assasins Creed Playstation
+		  (3,1,1,80),-- Smash
+		  (4,1,2,80),--Smash
+		  (5,13,3,20.90),--Sonic Mania
+		  (5,7,1,59.90),--Jedi Fallen Order
+		  (5,9,4,59.90),
+		  (6,16,2,20),--Terraformers
+		  (7,10,4,60),-- Watch Dogs
+		  (8,2,1,52.90),--Uncharted
+		  (9,17,2,19),--Scrap Mechanic
+		  (10,18,1,5);--Among us
 --END
