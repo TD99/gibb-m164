c5020d16b164c8f869ee77eacb465faa512f1606
olo135597 <olo135597@stud.gibb.ch>
2022-03-13 11:09:32 +0100

    Update Address.sql

--- Skripts/Student_001/Insert/Address.sql
+++ Skripts/Student_001/Insert/Address.sql
@@ -2,6 +2,21 @@
 
 --INSERT INTO ADDRESS
 	USE "test";
-	INSERT INTO "Address"(...)
-	VALUES(...);
+	INSERT INTO "Address"("FK_CustomerID", "FK_CityID", "Street", "StreetNr", "IsPrimaryAddress", "RemoveDate")
+	VALUES(1,1,'Lorrainestrasse','5b',1,NULL),
+	      (2,3,'Champs-élisées','50',1,NULL),
+		  (3,2,'Neuenbureger Str.','12',0,NULL),
+		  (3,4,'Via della Maglianella','4',1,NULL),
+		  (4,5,'Uchiha Str.','3',1,NULL),
+		  (5,1,'Spitalgasse','4',1,NULL),
+		  (6,2,'Gabrielenstrasse','6',1,NULL),
+		  (6,2,'Holwedestrasse','5',0,NULL),
+		  (7,6,'Othellogasse','7',1,NULL),
+		  (8,8,'Rua da Lapa','67',1,NULL),
+		  (9,7,'Carrer de Monjo','4',1,NULL),
+		  (10,10,'Cantina','2',1,NULL),
+		  (9,9,'Warwick Ave','3',0,NULL); 
+
+		  --SELECT FROM "Address"
+		  --DELETE FROM "Address"
 --END
