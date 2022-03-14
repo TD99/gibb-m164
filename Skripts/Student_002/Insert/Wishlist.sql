0f207e512b62a6f51c4b2e5d4e9fd21fd1cf286a
olo135597 <olo135597@stud.gibb.ch>
2022-03-14 13:24:41 +0100

    Update Wishlist.sql

--- Skripts/Student_002/Insert/Wishlist.sql
+++ Skripts/Student_002/Insert/Wishlist.sql
@@ -1,27 +1,47 @@
 --DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
 
 --INSERT INTO WISHLIST
-  USE test;
+USE test;
 
-  INSERT INTO "Wishlist"("FK_CustomerID","FK_ProductID")
-  VALUES(1,2),
-        (2,3),
-		(2,4),
-		(3,1),
-		(4,1),
-		(5,13),
-		(5,7),
-		(5,9),
-		(6,16),
-		(7,10),
-		(8,2),
-		(9,17),
-		(10,18),
-		(1,4),
-		(6,8),
-		(4,3),
-		(7,15),
-		(5,2),
-		(10,17);
+INSERT INTO "Wishlist"("FK_CustomerID","FK_ProductID")
+VALUES
+  --ROW 1
+	(1,2),
+  --ROW 2
+	(2,3),
+  --ROW 3
+	(2,4),
+  --ROW 4
+	(3,1),
+  --ROW 5
+	(4,1),
+  --ROW 6
+	(5,13),
+  --ROW 7
+	(5,7),
+  --ROW 8
+	(5,9),
+  --ROW 9
+	(6,16),
+  --ROW 10
+	(7,10),
+  --ROW 11
+	(8,2),
+  --ROW 12
+	(9,17),
+  --ROW 13
+	(10,18),
+  --ROW 14
+	(1,4),
+  --ROW 15
+	(6,8),
+  --ROW 16
+	(4,3),
+  --ROW 17
+	(7,15),
+  --ROW 18
+	(5,2),
+  --ROW 19
+    (10,17);
 		
 --END
