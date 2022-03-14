dfbb995415140718e3228b0ea1bcd114d1a866ac
olo135597 <olo135597@stud.gibb.ch>
2022-03-14 13:44:15 +0100

    Update Platform.sql

--- Skripts/Student_004/Insert/Platform.sql
+++ Skripts/Student_004/Insert/Platform.sql
@@ -3,20 +3,27 @@
 --INSERT INTO PLATFORM
 	USE "test";
 	INSERT INTO "Platform"("Name","Version","RemoveDate")
-	VALUES('Windows','XP','2020-05-03'),
-		  ('Windows','10',NULL),
-		  ('Windows','11',NULL),
-
-		  ('MacOS','Monterey',NULL),
-
-		  ('PlayStation','2','2019-05-19'),
-		  ('PlayStation','4',NULL),
-		  ('PlayStation','5',NULL),
-			
-		  ('XBOX','360','2022-03-07'),
-		  ('XBOX','One',NULL),
-			
-		  ('Nintendo','Switch',NULL);
+	VALUES
+	  --ROW 1 
+		('Windows','XP','2020-05-03'),
+	  --ROW 2
+		('Windows','10',NULL),
+	  --ROW 3
+		('Windows','11',NULL),
+	  --ROW 4
+		('MacOS','Monterey',NULL),
+      --ROW 5
+		('PlayStation','2','2019-05-19'),
+	  --ROW 6
+		('PlayStation','4',NULL),
+	  --ROW 7
+		('PlayStation','5',NULL),
+	  --ROW 8
+		('XBOX','360','2022-03-07'),
+	  --ROW 9
+		('XBOX','One',NULL),
+	  --ROW 10
+		('Nintendo','Switch',NULL);
 	/* Examples:
 	SELECT * FROM "Platform" ORDER BY "PlatformID" ASC;
 	*/
