5752b5c5c27ff367e5aa460c97bd7389d2072a9e
olo135597 <olo135597@stud.gibb.ch>
2022-03-14 13:32:36 +0100

    Update Publisher.sql

--- Skripts/Student_004/Insert/Publisher.sql
+++ Skripts/Student_004/Insert/Publisher.sql
@@ -3,25 +3,27 @@
 --INSERT INTO PUBLISHER
 	USE "test";
 	INSERT INTO "Publisher"("Name","Website","RemoveDate")
-	VALUES('Nintendo','https://www.nintendo.com/',NULL),
-
-		  ('Sony Interactive Entertainment','https://www.sie.com/','2020-05-05'),
-
-		  ('Tencent Games','https://game.qq.com/',NULL),
-
-		  ('Microsoft','https://www.microsoft.com/',NULL),
-
-		  ('Electronic Arts','https://www.ea.com/',NULL),
-
-		  ('Ubisoft','https://www.ubisoft.com/',NULL),
-
-		  ('Sega','https://www.sega.com/','2022-01-02'),
-
-		  ('Goblinzstudio','https://goblinzstudio.com/',NULL),
-
-		  ('Axolot Games','https://axolotgames.com/',NULL),
-
-		  ('Innersloth','https://www.innersloth.com/',NULL);
+	VALUES
+	  --ROW 1
+		('Nintendo','https://www.nintendo.com/',NULL),
+      --ROW 2
+		('Sony Interactive Entertainment','https://www.sie.com/','2020-05-05'),
+      --ROW 3
+		('Tencent Games','https://game.qq.com/',NULL),
+      --ROW 4
+		('Microsoft','https://www.microsoft.com/',NULL),
+      --ROW 5
+		('Electronic Arts','https://www.ea.com/',NULL),
+      --ROW 6
+		('Ubisoft','https://www.ubisoft.com/',NULL),
+      --ROW 7
+		('Sega','https://www.sega.com/','2022-01-02'),
+      --ROW 8
+		('Goblinzstudio','https://goblinzstudio.com/',NULL),
+      --ROW 9
+		('Axolot Games','https://axolotgames.com/',NULL),
+      --ROW 10
+		('Innersloth','https://www.innersloth.com/',NULL);
 	
 	/* Examples:
 	SELECT * FROM "Publisher" ORDER BY "PublisherID" ASC;
