8641e35c55dddf4eb101a9d2a1e090634ff266c5
T-Craft <tim@duerrs.ch>
2022-03-14 12:14:00 +0100

    Update InnerJoinEssential.sql

--- Skripts/Student_001/Joins/InnerJoinEssential.sql
+++ Skripts/Student_001/Joins/InnerJoinEssential.sql
@@ -1,16 +1,16 @@
 --DEFALUT USER IDENTIFIER: tdu135585 <-- DO NOT MODIFY
 
---INNER JOIN ALL Student_001
+--INNER JOIN ESSENTIAL Student_001
 	USE "test";
 	SELECT 
-		  --CUSTOMER
-			Customer.CustomerID,Customer.FirstName,Customer.LastName,Customer.Birthday,Customer.Gender,Customer.Email,Customer.PhoneHome,Customer.PhoneMobile,
-		  --ADDRESS
-			Address.Street,Address.StreetNr,Address.IsPrimaryAddress,
-		  --CITY
-			City.ZIPCode, City.Name AS 'City',
-		  --COUNTRY
-		    Country.Name AS 'Country'
+	  --CUSTOMER
+		"Customer"."CustomerID","Customer"."FirstName","Customer"."LastName","Customer"."Birthday","Customer"."Gender","Customer"."Email","Customer"."PhoneHome","Customer"."PhoneMobile",
+	  --ADDRESS
+		"Address"."Street","Address"."StreetNr","Address"."IsPrimaryAddress",
+	  --CITY
+		"City"."ZIPCode","City"."Name" AS 'City',
+	  --COUNTRY
+	    "Country"."Name" AS 'Country'
 	FROM "Customer"
 	INNER JOIN "Address" ON "Customer"."CustomerID" = "Address"."FK_CustomerID"
 	INNER JOIN "City" ON "Address"."FK_CityID" = "City"."CityID"
