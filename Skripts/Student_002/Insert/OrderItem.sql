c16ef770c5d8b3e8e5cd22c563b5c696dbbbe071
olo135597 <olo135597@stud.gibb.ch>
2022-03-14 13:13:08 +0100

    Update OrderItem.sql

--- Skripts/Student_002/Insert/OrderItem.sql
+++ Skripts/Student_002/Insert/OrderItem.sql
@@ -3,17 +3,31 @@
 --INSERT INTO ORDERITEM
 USE test;
 INSERT INTO "OrderItem"("FK_OrderID","FK_ProductID","Quantity","Price")
-	VALUES(1,2,2,52.90), --Uncharted
-	      (2,3,2,23), --Minecraft
-		  (2,4,1,59.99), --Assasins Creed Playstation
-		  (3,1,1,80),-- Smash
-		  (4,1,2,80),--Smash
-		  (5,13,3,20.90),--Sonic Mania
-		  (5,7,1,59.90),--Jedi Fallen Order
-		  (5,9,4,59.90),
-		  (6,16,2,20),--Terraformers
-		  (7,10,4,60),-- Watch Dogs
-		  (8,2,1,52.90),--Uncharted
-		  (9,17,2,19),--Scrap Mechanic
-		  (10,18,1,5);--Among us
+	VALUES
+	  --ROW 1
+		(1,2,2,52.90), --Uncharted
+	  --ROW 2
+		(2,3,2,23), --Minecraft
+	  --ROW 3
+		(2,4,1,59.99), --Assasins Creed Playstation
+	  --ROW 4
+		(3,1,1,80),-- Smash
+	  --ROW 5
+		(4,1,2,80),--Smash
+	  --ROW 6
+		(5,13,3,20.90),--Sonic Mania
+	  --ROW 7
+		(5,7,1,59.90),--Jedi Fallen Order
+	  --ROW 8
+		(5,9,4,59.90),
+	  --ROW 9
+		(6,16,2,20),--Terraformers
+	  --ROW 10
+		(7,10,4,60),-- Watch Dogs
+	  --ROW 11
+		(8,2,1,52.90),--Uncharted
+	  --ROW 12
+		(9,17,2,19),--Scrap Mechanic
+	  --ROW 13
+		(10,18,1,5);--Among us
 --END
