bf62855aaefc0f84619222d83b8e4633ea4105b6
T-Craft <tim@duerrs.ch>
2022-03-14 15:39:56 +0100

    ...

--- Skripts/Student_001/Exercises/DELETE.sql
+++ Skripts/Student_001/Exercises/DELETE.sql
@@ -1,7 +1,19 @@
---DEFALUT USER IDENTIFIER: olo135597 <-- DO NOT MODIFY
+--DEFALUT USER IDENTIFIER: tdu135585 <-- DO NOT MODIFY
 
 --DELETE DATA
 	USE "test";
-	DELETE FROM "PaymentMethod"
-		WHERE "Name" = 'Klarna';
+	/*DELETE FROM "Customer"
+		WHERE "FirstName" = 'Ishin';
+	DELETE FROM "Customer"
+		WHERE "Birthday" <= '2000-01-01';
+	
+	DELETE FROM "Country"
+		WHERE "Name" = 'South Africa';*/
+	DELETE FROM "Country"
+		WHERE "RemoveDate" IS NOT NULL;
+
+	/* Examples:
+	SELECT * FROM "Customer";
+	SELECT * FROM "Country";
+	*/
 --END
