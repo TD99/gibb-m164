e404f869b3538ddd5fe779c33d1cdeff4c9c2782
daviddresus <david.estevesrodrigues@bwdbern.ch>
2022-03-14 13:55:07 +0100

    Update Image.sql

--- Skripts/Student_004/Insert/Image.sql
+++ Skripts/Student_004/Insert/Image.sql
@@ -3,5 +3,42 @@
 --INSERT INTO IMAGE
 	USE "test";
 	INSERT INTO "Image"("FK_ProductID","ImageURL")
-	VALUES(1);
+	VALUES
+	  --ROW 1
+		(1, 'https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_SuperSmashBrosUltimate_02_image1600w.jpg'),	--smash bros
+	  --ROW 2
+		(2, 'https://image.api.playstation.com/vulcan/ap/rnd/202111/2001/9Vdas199J3tqxTIBWGijbpjW.png'),	--uncharted
+	  --ROW 3
+		(3, 'https://www.minecraft.net/content/dam/games/minecraft/key-art/Minecraft-xbox-one.jpg'),	--minecraft java edition
+	  --ROW 4
+		(4, 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6ATMRpo1frFP3DbIeyi9St/722d172a54dcb47a415f0d23092e194c/AC_CrossoverStories_KA_960x540_DE.jpg'),	--Assassin's creed
+	  --ROW 5
+		(5, 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6ATMRpo1frFP3DbIeyi9St/722d172a54dcb47a415f0d23092e194c/AC_CrossoverStories_KA_960x540_DE.jpg'),	--Assassin's creed
+	  --ROW 6
+		(6, 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6ATMRpo1frFP3DbIeyi9St/722d172a54dcb47a415f0d23092e194c/AC_CrossoverStories_KA_960x540_DE.jpg'),	--Assassin's creed
+	  --ROW 7
+	    (7,'https://image.api.playstation.com/vulcan/img/rnd/202105/1714/WHeOu95nW2SZQy6H5IKgE2Bg.png'),	--Jedi fallen order
+      --ROW 8
+	    (8,'https://image.api.playstation.com/vulcan/img/rnd/202105/1714/WHeOu95nW2SZQy6H5IKgE2Bg.png'),	--Jedi fallen order
+	  --ROW 9
+	    (9,'https://image.api.playstation.com/vulcan/img/rnd/202105/1714/WHeOu95nW2SZQy6H5IKgE2Bg.png'),	--Jedi fallen order
+      --ROW 10
+		(10, 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3ynMy93xlHyh1KG0U3NEmZ/5eee306e21753242ea88b027fe24f148/wd2-ubicom-game_info-connect_with_friends-v1.jpg'),	--Watch Dogs
+	  --ROW 11
+		(11, 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3ynMy93xlHyh1KG0U3NEmZ/5eee306e21753242ea88b027fe24f148/wd2-ubicom-game_info-connect_with_friends-v1.jpg'),	--Watch Dogs
+	  --ROW 12
+		(12, 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3ynMy93xlHyh1KG0U3NEmZ/5eee306e21753242ea88b027fe24f148/wd2-ubicom-game_info-connect_with_friends-v1.jpg'),	--Watch Dogs
+      --ROW 13
+		(13,'https://m.media-amazon.com/images/I/81AwSLaEBpL._SL1500_.jpg'),	--sonic mania
+	  --ROW 14
+		(14,'https://m.media-amazon.com/images/I/81AwSLaEBpL._SL1500_.jpg'),	--sonic mania
+	  --ROW 15
+		(15,'https://m.media-amazon.com/images/I/81AwSLaEBpL._SL1500_.jpg'),	--sonic mania
+	  --ROW 16
+		(16, 'https://pbs.twimg.com/media/EwiL0COXMAQzLgZ.jpg'),	--terraformers
+	  --ROW 17
+		(17, 'https://cdn.akamai.steamstatic.com/steam/apps/387990/capsule_616x353.jpg?t=1593703247'),	--Scrap mecanic
+	  --ROW 18
+		(18, 'https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_AmongUs.jpg');	--Among us
+		
 --END
