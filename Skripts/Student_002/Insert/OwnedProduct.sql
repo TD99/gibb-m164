e4cc479348c705e86dd55cb95bde361bfa0f8645
olo135597 <olo135597@stud.gibb.ch>
2022-03-14 13:16:52 +0100

    Update OwnedProduct.sql

--- Skripts/Student_002/Insert/OwnedProduct.sql
+++ Skripts/Student_002/Insert/OwnedProduct.sql
@@ -1,32 +1,59 @@
---DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
+	--DEFALUT USER IDENTIFIER: ile135624 <-- DO NOT MODIFY
 
 --INSERT INTO OWNEDPRODUCT
 	USE "test";
 	INSERT INTO "OwnedProduct"("FK_CustomerID","FK_ProductID","LicenceHash")
-	VALUES(1,2,'BASDFKJHKJ'),
-	      (1,2,'BASFJEKJEK'),
-	      (2,3,'BKBDFKJAHF'),
-		  (2,3,'JDIEWKWLEI'),
-		  (2,4,'HKJHDFUE3A'),
-		  (3,1,'FKJ478938A'),
-		  (4,1,'HGKJHD34GH'),
-		  (4,1,'HFJDFUEUJE'),
-		  (5,5,'asdlkfjIDi'),
-		  (5,5,'asdflkjIie'),
-		  (5,5,'HFuehasdfu'),
-		  (5,7,'LKJFDLJIFE'),
-		  (5,9,'lkLKFjeaf7'),
-		  (5,9,'asdlfkjoie'),
-		  (5,9,'asldfjiewo'),
-		  (5,9,'alsdfkjieo'),
-		  (6,3,'asdfieowlq'),
-		  (6,3,'fuejshdkfu'),
-		  (7,10,'asdfhuekjw'),
-		  (7,10,'fasdfjieow'),
-		  (7,10,'asdkflielw'),
-		  (7,10,'werikdiekd'),
-		  (8,2,'asdfielwold'),
-		  (9,8,'JFLJ30LFUH'),
-		  (9,8,'DJIEHGNGHE'),
-		  (10,6,'FHKJDUEH7J');
+	VALUES
+	  --ROW 1
+		(1,2,'BASDFKJHKJ'),
+	  --ROW 2
+		(1,2,'BASFJEKJEK'),
+	  --ROW 3
+		(2,3,'BKBDFKJAHF'),
+	  --ROW 4
+		(2,3,'JDIEWKWLEI'),
+	  --ROW 5
+		(2,4,'HKJHDFUE3A'),
+	  --ROW 6
+		(3,1,'FKJ478938A'),
+	  --ROW 7
+		(4,1,'HGKJHD34GH'),
+	  --ROW 8
+		(4,1,'HFJDFUEUJE'),
+	  --ROW 9
+		(5,5,'asdlkfjIDi'),
+	  --ROW 10
+		(5,5,'asdflkjIie'),
+	  --ROW 11
+		(5,5,'HFuehasdfu'),
+	  --ROW 12
+		(5,7,'LKJFDLJIFE'),
+	  --ROW 13
+		(5,9,'lkLKFjeaf7'),
+	  --ROW 14
+		(5,9,'asdlfkjoie'),
+	  --ROW 15
+		(5,9,'asldfjiewo'),
+	  --ROW 16
+		(5,9,'alsdfkjieo'),
+	  --ROW 17
+		(6,3,'asdfieowlq'),
+	  --ROW 18
+		(6,3,'fuejshdkfu'),
+	  --ROW 19
+		(7,10,'asdfhuekjw'),
+	  --ROW 20
+		(7,10,'fasdfjieow'),
+	  --ROW 21
+		(7,10,'asdkflielw'),
+	  --ROW 22
+		(7,10,'werikdiekd'),
+	  --ROW 23
+		(8,2,'asdfielwold'),
+	  --ROW 24
+		(9,8,'JFLJ30LFUH'),
+	  --ROW 25
+		(9,8,'DJIEHGNGHE'),
+	  --ROW 26
+		(10,6,'FHKJDUEH7J');
 --END
