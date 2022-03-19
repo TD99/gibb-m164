6c4edf6e0038446c5f4ce8ce12e177991c35a5ad
olo135597 <olo135597@stud.gibb.ch>
2022-03-19 18:01:54 +0100

    Update BULKINSERT.sql

--- Skripts/Student_003/Exercises/BULKINSERT.sql
+++ Skripts/Student_003/Exercises/BULKINSERT.sql
@@ -1,17 +1,12 @@
 --DEFALUT USER IDENTIFIER: olo135597 <-- DO NOT MODIFY
 
 --BULKINSERT
-USE "test"; 
-
- BULK INSERT BInsert
-   FROM 'C:\Daten\gibb-m164\Skripts\Student_003\Exercises\covid19.csv'
-   WITH (DATAFILETYPE = 'char', 
-         FIRSTROW = 2,
-         FIELDTERMINATOR = ';',
-	     ROWTERMINATOR = '0x0a',
-         KEEPIDENTITY,
-         KEEPNULLS
- );
+bulk insert "Country"
+from 'C:\Daten\gibb-m164\Zusatzmaterial\country.csv'
+with
+(
+rowterminator='\n',
+fieldterminator=','
 
 	
 --END
