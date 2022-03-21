30ab87803f21ce74b7f669f52f6f22683da53c06
olo135597 <olo135597@stud.gibb.ch>
2022-03-21 13:26:54 +0100

    Update Product.sql

--- Skripts/Student_003/Exercises/SELECT/Product.sql
+++ Skripts/Student_003/Exercises/SELECT/Product.sql
@@ -9,12 +9,13 @@
 	SELECT "Name", "Price", "MinAge"
  FROM "Product"
  WHERE "FK_CategoryID" = (SELECT "CategoryID" FROM "Category"
- WHERE "Name" = 'Adventure');*/
+ WHERE "CategoryID" = '3');*/
 
 	/*USE "test";
 	UPDATE "Product"
  SET "FK_CategoryID" = (SELECT "CategoryID" FROM "Category"
  WHERE "Name" = 'RPG')
  WHERE "FK_CategoryID" = (SELECT "CategoryID" FROM "Category"
- WHERE "Name" = 'Sandbox');*/
+ WHERE "Name" = 'Sandbox');*/ 
+
 --END
