a2f1e019b4c094177284add20216b533bd9584d3
Tim Dürr <tim@duerrs.ch>
2022-04-03 13:57:54 +0200

    ...

--- Skripts/Student_002/Exercises/BULKINSERT.sql
+++ Skripts/Student_002/Exercises/BULKINSERT.sql
@@ -1,17 +1,19 @@
---DEFALUT USER IDENTIFIER: olo135597 <-- DO NOT MODIFY
-
 --BULKINSERT
-	USE lernende;
-	CREATE TABLE covid19(
-		Country VARCHAR(255),
-		Totalcases VARCHAR(255),
-		Totaldeaths VARCHAR(255))
-	bulk insert "covid19"
-	from 'C:\Daten\gibb-m164\Zusatzmaterial\covid19.csv'
-	with
-	(
-	rowterminator='\n',
-	fieldterminator=','
+	USE "test";
+	CREATE TABLE "covid19"(
+		"Country" VARCHAR(255),
+		"Totalcases" VARCHAR(255),
+		"Totaldeaths" VARCHAR(255)
 	);
+	BULK INSERT "covid19"
+		FROM 'C:\Daten\gibb-m164\Zusatzmaterial\covid19.csv' -- Default Path (Can be different on your system!)
+		WITH(
+		FIRSTROW=2,
+		FIELDTERMINATOR=',',
+		ROWTERMINATOR='\n'
+		);
+	
+	/* Examples
 	SELECT * FROM covid19;
+	*/
 --END
